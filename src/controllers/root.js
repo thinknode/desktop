e88a3eeddfbf49aef73b10fa2c7b81f93083b51a
kburnett <kburnett@thinknode.com>
2016-07-22 13:56:56 -0400

    Version bump to 1.2.0

--- src/controllers/root.js
+++ src/controllers/root.js
@@ -46,7 +46,7 @@
         $scope.init = function() {
             var msg = '<span class="updateLink" onclick="helpers.openDownloadPage()">' +
                 'A new version is available for download: Click here to download</span>';
-            var version = "1.1.0";
+            var version = "1.2.0";
             var request = $http({
                 method: "get",
                 url: "https://s3.amazonaws.com/thinknode-desktop-client/current.json"
